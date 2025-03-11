0435c3546ea793a308816affc07d78b2390cdf6e
Claudio Dettoni <claudio.dettoni@reaktor.com>
2025-03-11 13:07:34 +0000

    fix angularls with nx by setting root_dir

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,45 +1,51 @@
-local lsp_configurations = {
-  elixirls = {
-    cmd = { "/Users/dettonijr/elixir-ls/language_server.sh" },
-  },
-  ts_ls = {},
-  lua_ls = {
-    on_init = function(client)
-      --if client.workspace_folders then
-      --  local path = client.workspace_folders[1].name
-      --  if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      --    return
-      --  end
-      --end
+local lsp_configurations = function()
+  local util = require('lspconfig.util')
+  return {
+    elixirls = {
+      cmd = { "/Users/dettonijr/elixir-ls/language_server.sh" },
+    },
+    ts_ls = {},
+    lua_ls = {
+      on_init = function(client)
+        --if client.workspace_folders then
+        --  local path = client.workspace_folders[1].name
+        --  if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+        --    return
+        --  end
+        --end
 
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        runtime = {
-          -- Tell the language server which version of Lua you're using
-          -- (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT'
-        },
-        -- Make the server aware of Neovim runtime files
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            vim.env.VIMRUNTIME,
-            -- Depending on the usage, you might want to add additional paths here.
-            "${3rd}/luv/library"
-            -- "${3rd}/busted/library",
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+              -- Depending on the usage, you might want to add additional paths here.
+              "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+            -- library = vim.api.nvim_get_runtime_file("", true)
           }
-          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-          -- library = vim.api.nvim_get_runtime_file("", true)
-        }
-      })
-    end,
-    settings = {
-      Lua = {}
+        })
+      end,
+      settings = {
+        Lua = {}
+      }
+    },
+    clangd = {
+      cmd = { "clangd", "--compile-commands-dir=." },
+    },
+    angularls = {
+      root_dir = util.root_pattern('angular.json', 'project.json')
     }
-  },
-  clangd = {
-    cmd = { "clangd", "--compile-commands-dir=." },
   }
-}
+end
 
 return {
   -- Repo and package manager with LSPs, linters, formatters, DAPs
@@ -200,7 +206,7 @@ return {
       -- Set up lspconfig.
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-      for lsp, setup in pairs(lsp_configurations) do
+      for lsp, setup in pairs(lsp_configurations()) do
         setup[capabilities] = capabilities
         require('lspconfig')[lsp].setup(setup)
       end
