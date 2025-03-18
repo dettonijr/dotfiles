7a8233d8688488dc90e1d84632a57150d3d35662
Claudio Dettoni <cd611@cl.cam.ac.uk>
2025-03-18 19:44:29 +0000

    westerm and init.sh

--- init.sh
+++ init.sh
@@ -9,6 +9,7 @@ brew install cmake
 
 ln -s $HOME/dotfiles/.profile $HOME || true 
 ln -s $HOME/dotfiles/.wezterm.lua $HOME || true
+ln -s $HOME/dotfiles/.gitconfig $HOME || true
 
 mkdir -p $HOME/.config
 ln -s $HOME/dotfiles/.config/nvim $HOME/.config || true
