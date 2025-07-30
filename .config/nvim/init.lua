5686941277b1e9d2a2249778cbae853caa3b6c67
Claudio Dettoni <cd611@cl.cam.ac.uk>
2025-07-30 20:08:14 +0100

    move keymaps to file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,7 +14,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.cursorline = true
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
@@ -22,29 +22,28 @@ vim.opt.foldlevel = 99
 vim.opt.foldnestmax = 4
 vim.opt.foldcolumn = "0"
 
+vim.api.nvim_create_autocmd("BufReadPost", { command = 'silent! normal! g`"zz' })
+vim.keymap.set("n", "<C-j>", "5<C-e>5j")
+vim.keymap.set("n", "<C-k>", "5<C-y>5k")
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
 
-vim.api.nvim_create_autocmd('BufReadPost', { command = "silent! normal! g`\"zz" })
-vim.keymap.set('n', '<C-j>', '5<C-e>5j')
-vim.keymap.set('n', '<C-k>', '5<C-y>5k')
-vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
-
-
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Auto-install lazy.nvim if not present
 if not vim.uv.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-  print('Done.')
+	print("Installing lazy.nvim....")
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+	print("Done.")
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup("plugins")
+require("lazy").setup("plugins")
+require("keymaps")
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -87,7 +87,7 @@ return {
     config = function()
       require("conform").setup({
         formatters_by_ft = {
-          lua = { lsp_format = "prefer" },
+          lua = { "stylua" },
           -- Conform will run multiple formatters sequentially
           python = { "isort", "black" },
           -- You can customize some of the format options for the filetype (:help conform.format)
@@ -138,33 +138,6 @@ return {
       -- This will avoid an annoying layout shift in the screen
       vim.opt.signcolumn = 'yes'
 
-      -- This is where you enable features that only work
-      -- if there is a language server active in the file
-      vim.api.nvim_create_autocmd('LspAttach', {
-        desc = 'LSP actions',
-        callback = function(event)
-          local opts = { buffer = event.buf }
-
-          vim.keymap.set('n', '<leader>ch', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end)
-
-          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-          vim.keymap.set('n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-          -- vim.keymap.set({ 'n', 'x' }, '<leader>cf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-          vim.keymap.set({ 'n', 'x' }, '<leader>cf', '<cmd>lua require("conform").format({async = true})<cr>', opts)
-          vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-          vim.keymap.set('n', '<leader>cd', ':lua vim.diagnostic.open_float(0, {scope="line"})<cr>', opts)
-          vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
-          vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
-        end,
-      })
     end
   },
 
