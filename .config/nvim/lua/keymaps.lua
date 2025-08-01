c7a5274747bbef8dca0b040c97f9ebf1321e1843
Claudio Dettoni <cd611@cl.cam.ac.uk>
2025-08-01 20:49:15 +0100

    clean up

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,40 +1,30 @@
--- This is where you enable features that only work
--- if there is a language server active in the file
+local map = vim.keymap.set
+
 vim.api.nvim_create_autocmd("LspAttach", {
   desc = "LSP actions",
   callback = function(event)
     local opts = { buffer = event.buf }
 
-    vim.keymap.set("n", "<leader>ch", function()
+    map("n", "<leader>ch", function()
       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
     end)
 
-    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-    vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-    vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-    vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-    vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-    vim.keymap.set("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-    -- vim.keymap.set({ 'n', 'x' }, '<leader>cf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', vim.tbl_extend('force', opts, { desc = '' }))
-    vim.keymap.set(
-      { "n", "x" },
-      "<leader>cf",
-      '<cmd>lua require("conform").format({async = true})<cr>',
-      vim.tbl_extend("force", opts, { desc = "" })
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>ca",
-      "<cmd>lua vim.lsp.buf.code_action()<cr>",
-      vim.tbl_extend("force", opts, { desc = "" })
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>cd",
-      ':lua vim.diagnostic.open_float(0, {scope="line"})<cr>',
-      vim.tbl_extend("force", opts, { desc = "" })
-    )
+    map("n", "K", vim.lsp.buf.hover, opts)
+    map("n", "gd", vim.lsp.buf.definition, opts)
+    map("n", "gD", vim.lsp.buf.declaration, opts)
+    map("n", "gi", vim.lsp.buf.implementation, opts)
+    map("n", "go", vim.lsp.buf.type_definition, opts)
+    map("n", "gr", vim.lsp.buf.references, opts)
+    map("n", "gs", vim.lsp.buf.signature_help, opts)
+    map("n", "<leader>cr", vim.lsp.buf.rename, opts)
+    -- km({ 'n', 'x' }, '<leader>cf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', vim.tbl_extend('force', opts, { desc = '' }))
+    map({ "n", "x" }, "<leader>cf", function()
+      require("conform").format({ async = true })
+    end, vim.tbl_extend("force", opts, { desc = "" }))
+    map("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "" }))
+
+    map("n", "<leader>cd", function()
+      vim.diagnostic.open_float({ float_bufnr = 0, scope = "line" })
+    end, vim.tbl_extend("force", opts, { desc = "" }))
   end,
 })
