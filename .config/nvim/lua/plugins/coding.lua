75dde48a48d96216eb32c8e50b6b96d695eba224
Claudio Dettoni <cd611@cl.cam.ac.uk>
2025-08-02 20:16:01 +0100

    trouble

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -19,6 +19,30 @@ return {
   },
 
   {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+    },
+  },
+
+  {
     "dlvandenberg/tree-sitter-angular",
     enabled = vim.g.vscode == nil,
   },
@@ -109,7 +133,7 @@ return {
       input = { enabled = true },
       picker = {
         enabled = true,
-        layout = { preset = "ivy" },
+        -- layout = { preset = "telescope", layout = { position = "top" } },
         matcher = {
           frecency = true,
         },
